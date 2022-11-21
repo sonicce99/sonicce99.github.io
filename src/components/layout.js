bd7bb8ff3334b4f3cbb43a8331f258801dd4938a
dongsu <sonicce99@naver.com>
2022-11-21 13:52:48 +0900

    feat: footer comment 변경

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,9 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © {new Date().getFullYear()}, Welcome to my Github
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <a href="https://github.com/sonicce99">DongSu Lee</a>
       </footer>
     </div>
   )
