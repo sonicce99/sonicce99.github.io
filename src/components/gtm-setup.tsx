5fb81ab778657dcf6db0ecd1b2867077f2bc40bb
dongsulee <dongsulee@nextedition.co.kr>
2023-10-31 11:23:46 +0900

    feat : 코드수정

--- src/components/gtm-setup.tsx
+++ src/components/gtm-setup.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { oneLine } from "common-tags"
 
-const gtm: string = "GTM-XXXXXXX"
+const gtm: string = "GTM-M5W59LM2"
 
 // Google Tag Manager (head)
 export const gtmScript = (
@@ -9,13 +9,11 @@ export const gtmScript = (
     key="gtm-js"
     dangerouslySetInnerHTML={{
       __html: oneLine`
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-M5W59LM2');</script>
-    <!-- End Google Tag Manager -->
+    })(window,document,'script','dataLayer',${gtm});
 `,
     }}
   />
@@ -27,10 +25,8 @@ export const gtmNoscript = (
     key="gtm-noscript"
     dangerouslySetInnerHTML={{
       __html: oneLine`
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5W59LM2"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
+    <iframe src="https://www.googletagmanager.com/ns.html?id=${gtm}"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>
 `,
     }}
   />
