0b8497d2e571a81a1a6577447c7f8529ed3df6c0
dongsu <sonicce99@naver.com>
2024-02-04 14:34:58 +0900

    feat : gatsby-remark-images maxWidth 주석처리

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 630,
+              // maxWidth: 630,
             },
           },
           {
